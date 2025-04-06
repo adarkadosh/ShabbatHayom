925e32c1f2a8b4de4977ec6cbb3c106ee886b073
omerelhadad2000 <omer.elh11@gmail.com>
2025-04-06 18:36:36 +0300

    Update UI

--- Assets/Folders By Name/Omer/Scripts/Pools/Spawner.cs
+++ Assets/Folders By Name/Omer/Scripts/Pools/Spawner.cs
@@ -5,8 +5,7 @@ public class Spawner : MonoBehaviour
 {
     // Update is called once per frame
     [SerializeField] private Transform _spawnPoint;
-
-
+    
     private void OnEnable()
     {
         GameEvents.OnProductCollected += SpawnItem;
@@ -15,6 +14,12 @@ public class Spawner : MonoBehaviour
     {
         var item = ItemPool.Instance.Get();
         item.transform.position = _spawnPoint.position;
+    }
+    
+    private void SetSprite(Sprite sprite)
+    {
+        var item = ItemPool.Instance.Get();
+        item.GetComponent<SpriteRenderer>().sprite = sprite;
     }
     
     private void OnDisable()
---
+++ Assets/Folders By Name/Omer/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class SoundManager : MonoSingleton<SoundManager>
+{
+    [SerializeField] private AudioClip playerHitSound;
+    [SerializeField] private AudioClip itemSpawnSound;
+    [SerializeField] private AudioClip backgroundMusic;
+    [SerializeField] private AudioClip timerSound;
+    [SerializeField] private AudioClip togglePauseSound;
+    private float pitchIncrement = 0.1f;
+
+    
+
+    private AudioSource backgroundMusicSource;
+    private AudioSource soundEffectsSource;
+
+    private void Awake()
+    {
+        backgroundMusicSource = gameObject.AddComponent<AudioSource>();
+        soundEffectsSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        // GameManager.Instance.GameStart += OnGameStart;
+        // GameManager.Instance.GamePause += OnGamePause;
+        // GameManager.Instance.GameResume += OnGamePause;
+        OnGameStart(); //Move to the game manager
+        GameEvents.OnProductCollected += OnItemSpawned;
+        
+        
+    }
+    
+    private void OnDifficultyChanged()
+    {
+        backgroundMusicSource.pitch += pitchIncrement;
+    }
+
+    private void OnGameStart()
+    {
+        PlayBackgroundMusic(backgroundMusic);
+    }
+
+    private void PlayBackgroundMusic(AudioClip clip)
+    {
+        backgroundMusicSource.clip = clip;
+        backgroundMusicSource.loop = true;
+        backgroundMusicSource.Play();
+    }
+    
+    private void OnGamePause()
+    {
+        PlaySoundEffect(togglePauseSound);
+    }
+    
+    private void OnItemSpawned()
+    {
+        PlaySoundEffect(itemSpawnSound);
+    }
+
+    private void PlaySoundEffect(AudioClip clip)
+    {
+        soundEffectsSource.PlayOneShot(clip);
+    }
+
+    private void OnDisable()
+    {
+        // GameManager.Instance.GameStart -= OnGameStart;
+        // GameManager.Instance.GamePause -= OnGamePause;
+        // GameManager.Instance.GameResume -= OnGamePause;
+        GameEvents.OnProductCollected -= OnItemSpawned;
+    }
+}
