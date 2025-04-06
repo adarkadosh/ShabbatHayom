dd4e669cc9f43f02d9275eae922c56b66808fbb4
Itai Muntner <itai.muntner@mail.huji.ac.il>
2025-04-06 22:38:55 +0300

    Created deque for tetris

---
+++ Assets/General Scripts/ProductDeque.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class ProductDeque : MonoBehaviour
+{
+    private readonly LinkedList<Products> _deque = new LinkedList<Products>();
+    
+    public void AddFront(Products productType)
+    {
+        _deque.AddFirst(productType);
+    }
+    
+    public void PushBack(Products productType)
+    {
+        _deque.AddLast(productType);
+    }
+    
+    public Products PopFront()
+    {
+        if (_deque.Count == 0)
+            throw new InvalidOperationException("Deque is empty");
+        
+        var product = _deque.First.Value;
+        _deque.RemoveFirst();
+        return product;
+    }
+    
+    public Products RemoveBack()
+    {
+        if (_deque.Count == 0)
+            throw new InvalidOperationException("Deque is empty");
+        
+        var product = _deque.Last.Value;
+        _deque.RemoveLast();
+        return product;
+    }
+    
+    public Products PeekFront()
+    {
+        if (_deque.Count == 0)
+            throw new InvalidOperationException("Deque is empty");
+        
+        return _deque.First.Value;
+    }
+    
+    public Products PeekBack()
+    {
+        if (_deque.Count == 0)
+            throw new InvalidOperationException("Deque is empty");
+        
+        return _deque.Last.Value;
+    }
+    
+    public int Count => _deque.Count;
+    
+    public bool IsEmpty => _deque.Count == 0;
+    
+    public void Clear()
+    {
+        _deque.Clear();
+    }
+    
+    public void PrintDeque()
+    {
+        foreach (var item in _deque)
+        {
+            Debug.Log(item);
+        }
+    }
+    
+    public void PrintDequeReverse()
+    {
+        for (var node = _deque.Last; node != null; node = node.Previous)
+        {
+            Debug.Log(node.Value);
+        }
+    }
+}
